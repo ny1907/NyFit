65e7bc2bcdc2c21ce5e319873c85da76db6083d6
U820319 <U820319@LTLS-W43653.ads.dlh.de>
2016-10-14 17:00:04 +0200

    Einzelansicht angepasst

--- app/src/main/java/ny/nyfit/ListViewActivityItem.java
+++ app/src/main/java/ny/nyfit/ListViewActivityItem.java
@@ -3,7 +3,9 @@ package ny.nyfit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 /**
@@ -18,13 +20,23 @@ public class ListViewActivityItem extends AppCompatActivity{
     Float proteine;
     Float fett;
 
-    TextView tname;
-    TextView tkcal;
-    TextView tkohlenhydrate;
-    TextView tproteine;
-    TextView tfett;
+    private TextView tname;
+    private TextView tkcal;
+    private TextView tkohlenhydrate;
+    private TextView tproteine;
+    private TextView tfett;
 
-    EditText eName;
+    private EditText eName;
+    private EditText eKcal;
+    private EditText eKohlenhydrate;
+    private EditText eProteine;
+    private EditText eFett;
+
+    private ImageButton editButton;
+    private ImageButton saveButton;
+    private ImageButton discardButton;
+    private ImageButton deleteButton;
+
 
     protected void onCreate(final Bundle savedInstanBundle){
         super.onCreate(savedInstanBundle);
@@ -40,7 +52,18 @@ public class ListViewActivityItem extends AppCompatActivity{
         tproteine = (TextView)findViewById(R.id.proteine);
         tfett = (TextView)findViewById(R.id.fett);
 
-        eName = (EditText)findViewById(R.id.editkohlenhydrate);
+        // IDs der Eingabefelder
+        eName = (EditText)findViewById(R.id.editname);
+        eKcal = (EditText)findViewById(R.id.editkcal);
+        eKohlenhydrate = (EditText)findViewById(R.id.editkohlenhydrate);
+        eProteine = (EditText)findViewById(R.id.editproteine);
+        eFett = (EditText)findViewById(R.id.editfett);
+
+        // IDs der Buttons
+        editButton = (ImageButton)findViewById(R.id.editButton);
+        saveButton = (ImageButton)findViewById(R.id.saveButton);
+        discardButton = (ImageButton)findViewById(R.id.discardButton);
+        deleteButton = (ImageButton)findViewById(R.id.deleteButton);
 
         // Datenbankverbindung öffnen
         MySQLiteHelper db = new MySQLiteHelper(this);
@@ -65,8 +88,82 @@ public class ListViewActivityItem extends AppCompatActivity{
         db.close();
     }
 
-    public void editFood(){
+    public void editFood(View view){
+        // Buttons unsichtbar machen
+        editButton.setVisibility(ImageButton.INVISIBLE);
+        deleteButton.setVisibility(ImageButton.INVISIBLE);
+
+        // Textfelder unsichtbar machen
+        tname.setVisibility(TextView.INVISIBLE);
+        tkcal.setVisibility(TextView.INVISIBLE);
+        tkohlenhydrate.setVisibility(TextView.INVISIBLE);
+        tproteine.setVisibility(TextView.INVISIBLE);
+        tfett.setVisibility(TextView.INVISIBLE);
+
+        // Eingabefelder vorbefüllen
         eName.setText(this.name);
+        eKcal.setText(this.kcal.toString());
+        eKohlenhydrate.setText(this.kohlenhydrate.toString());
+        eProteine.setText(this.proteine.toString());
+        eFett.setText(this.fett.toString());
+
+        // Buttons sichtbar machen
+        saveButton.setVisibility(ImageButton.VISIBLE);
+        discardButton.setVisibility(ImageButton.VISIBLE);
+
+        // Eingabefelder sichtbar machen
         eName.setVisibility(EditText.VISIBLE);
+        eKcal.setVisibility(EditText.VISIBLE);
+        eKohlenhydrate.setVisibility(EditText.VISIBLE);
+        eProteine.setVisibility(EditText.VISIBLE);
+        eFett.setVisibility(EditText.VISIBLE);
+    }
+
+    // Discard Changes
+    public void discardChanges(View view){
+        // Buttons unsichtbar machen
+        discardButton.setVisibility(ImageButton.INVISIBLE);
+        saveButton.setVisibility(ImageButton.INVISIBLE);
+
+        // Eingabefelder unsichtbar machen
+        eName.setVisibility(ImageButton.INVISIBLE);
+        eKcal.setVisibility(ImageButton.INVISIBLE);
+        eKohlenhydrate.setVisibility(ImageButton.INVISIBLE);
+        eProteine.setVisibility(ImageButton.INVISIBLE);
+        eFett.setVisibility(ImageButton.INVISIBLE);
+
+        // Eingabefelder leeren
+        eName.setText("");
+        eKcal.setText("");
+        eKohlenhydrate.setText("");
+        eProteine.setText("");
+        eFett.setText("");
+
+        // Textfelder sichtbar machen
+        tname.setVisibility(TextView.VISIBLE);
+        tkcal.setVisibility(TextView.VISIBLE);
+        tkohlenhydrate.setVisibility(TextView.VISIBLE);
+        tproteine.setVisibility(TextView.VISIBLE);
+        tfett.setVisibility(TextView.VISIBLE);
+
+        // Buttons sichtbar machen
+        editButton.setVisibility(ImageButton.VISIBLE);
+        deleteButton.setVisibility(ImageButton.VISIBLE);
+    }
+
+    // Delete Food from Database
+    public void deleteFood(View view){
+
+        //TODO Dialog ob sicher
+        MySQLiteHelper db = new MySQLiteHelper(this);
+        db.getWritableDatabase();
+
+        db.delete(this.food);
+        db.close();
+
+        super.onBackPressed();
+    }
+
+    public void saveChanges(View view) {
     }
 }
